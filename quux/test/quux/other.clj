3f895d6c4d7c085adb5a8cc51f3b71589fb531da
Larry Jones <mrwizard82d1@gmail.com>
2018-04-14 12:17:49 -0500

    Add tests for checkers

---
+++ quux/test/quux/other.clj
@@ -0,0 +1,31 @@
+(ns quux.other
+  (:require [midje.sweet :refer :all]))
+
+(println "Testing checkers")
+
+;; Although this form works, ...
+(fact (+ 1 2) even?)
+;; ... as does this form, ...
+(fact (some even? [1 5]) => nil)
+;; ... these forms are more expressive.
+(fact (some even? [1 5]) => falsey)
+
+;; Negating a check
+(fact (some even? [1 5]) =not=> truthy)
+
+;; We can use extended equality to write code the way people speak, ...
+(fact
+  [1 3 5 8] => #(some even? %)
+  [1 3 5 8] => (partial some even?))
+;; ... but Midje provides checkers to make it even more obvious
+(fact [1 3 5 8] => (contains even?))
+
+;; (do
+;;   (println "Two expected failures")
+;;   (fact {:a 1 :b 2} => (just {:a 1 :CCC 333}))
+;;   (fact [1 2 3 4 5] => (contains [1 2 4])))
+
+;; But allowing gaps succeeds
+(fact [1 2 3 4 5] => (contains [1 2 4] :gaps-ok))
+
+
